4940715ad4c3551e7be3c475b5b358dce3888983
Danh-m1 <danh2k@nexondv.com>
2024-04-07 17:08:01 +0700

    feat: logic check pair card - 2 style non watch vs watch

--- Ch1-pokemon/assets/js/main.js
+++ Ch1-pokemon/assets/js/main.js
@@ -2,18 +2,70 @@ const { createApp, ref, watch } = Vue
 
 createApp({
   setup() {
-    const selectedOption = ref(4)
+    const selectedOption = ref(8)
     const deck = ref(createDeck(selectedOption.value))
+    const currentPair = []
+    const pairRef = ref([])
 
     // In this static render there flash of image slow load from api
     const selectCard = (card) => {
-      console.log("🚀 main L11-selectCard", card)
+      // prevent select more when 2 cards are in animation checking
+      const styleCheck = pairRef.value
+      // const styleCheck = currentPair
+
+      if (styleCheck.length === 2) {
+        return
+      }
+
       card.isCovered = !card.isCovered
+      styleCheck.push(card)
+
+      // because mutation of the same object
+      // no need to loop find in deck
+      // 1st timeout to finish the animation
+      // need calculate animation time
+
+      // if (currentPair.length < 2) return
+
+      // setTimeout(() => {
+      //   const [card1, card2] = currentPair
+      //   if (card1.name !== card2.name) {
+      //     card1.isCovered = true
+      //     card2.isCovered = true
+      //     // this timeout to wait for 2 card covered back
+      //     setTimeout(() => {
+      //       currentPair.length = 0
+      //     }, 500)
+      //   } else {
+      //     currentPair.length = 0
+      //   }
+      // }, 500)
+      // hard logic -> switch watch
     }
 
     watch(selectedOption, (newValue) => {
       console.log("🚀 main L15-newValue", newValue, typeof newValue)
     })
+
+    // cannot watch non-reactive value like currentPair
+    watch(
+      () => pairRef.value.length,
+      () => {
+        const refValue = pairRef.value
+        if (refValue.length !== 2) return
+        const [card1, card2] = refValue
+        if (card1.name === card2.name) {
+          refValue.length = 0
+          return
+        }
+
+        setTimeout(() => {
+          card1.isCovered = true
+          card2.isCovered = true
+          refValue.length = 0
+        }, 600)
+      }
+    )
 
     return {
       selectedOption,
